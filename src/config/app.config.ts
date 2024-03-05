984f77ca55c0acc08af6da2aec32fb18667bc5fe
Carles <carmar85@gmail.com>
2024-03-05 19:21:33 +0100

    env config

--- src/config/app.config.ts
+++ src/config/app.config.ts
@@ -2,6 +2,6 @@
 export const EnvConfiguration = () => ({
     environment: process.env.NODE_ENV || 'dev',
     mongo_db: process.env.MONGO_DB,
-    port: process.env.PORT || 3001,
+    port: process.env.PORT || 3000,
     default_limit: process.env.DEFAULT_LIMIT || 10
 })
